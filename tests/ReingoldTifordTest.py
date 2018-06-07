36550159b6d3f91ef85d61b2c92f99d6472d6be6
Nickan <colete_nico@yahoo.com>
2018-06-07 23:16:57 +0800

    Fixed depth testing in checkContourNode4() checkContourNode3()

--- tests/ReingoldTifordTest.py
+++ tests/ReingoldTifordTest.py
@@ -13,175 +13,175 @@ class ReingoldTilfordTest(unittest.TestCase):
   def __init__(self, methodName='runTest'):
     unittest.TestCase.__init__(self, methodName=methodName)
   
-#   def testLocalXRelativeToParentNode(self):
-#     filePath = Utils.getAssetPath("test/setRelativeXToParent.json")
-#     nodeList = SaveManager.loadNodeDataListKeyConvertedToInt(filePath)
-#     
-#     reingold = ReingoldTilford()
-#     coords, checkedForConflictIds = reingold.setInitialX(nodeList[1], nodeList)
-#       
-#     name = "1"  # Main node
-#     node = coords[int(name)]
-#     self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
-#     self.assertTrue(self.verifyNodeX(node, 2), name + " x is wrong")
-#       
-#     # Main node children
-#     name = "2"
-#     node = coords[int(name)]
-#     self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
-#     self.assertTrue(self.verifyNodeX(node, 1), name + " x is wrong")
-#       
-#     name = "3"
-#     node = coords[int(name)]
-#     self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
-#     self.assertTrue(self.verifyNodeX(node, 2), name + " x is wrong")
-#     self.assertTrue(self.verifyMod(node, 2), name + " mod is wrong")
-#       
-#     name = "4"
-#     node = coords[int(name)]
-#     self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
-#     self.assertTrue(self.verifyNodeX(node, 3), name + " x is wrong")
-#     self.assertTrue(self.verifyMod(node, 2.5), name + " mod is wrong")
-#     # Main node children
-#       
-#     # Node 2 children
-#     name = "5"
-#     node = coords[int(name)]
-#     self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
-#     self.assertTrue(self.verifyNodeX(node, 0), name + " x is wrong")
-#       
-#     name = "6"
-#     node = coords[int(name)]
-#     self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
-#     self.assertTrue(self.verifyNodeX(node, 1), name + " x is wrong")
-#       
-#     name = "7"
-#     node = coords[int(name)]
-#     self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
-#     self.assertTrue(self.verifyNodeX(node, 2), name + " x is wrong")
-#     # Node 2 children
-#       
-#     # Node 3 child
-#     name = "8"
-#     node = coords[int(name)]
-#     self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
-#     self.assertTrue(self.verifyNodeX(node, 0), name + " x is wrong")
-#       
-#       
-#     # Node 4 children
-#     name = "9"
-#     node = coords[int(name)]
-#     self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
-#     self.assertTrue(self.verifyNodeX(node, 0), name + " x is wrong")
-#       
-#     name = "10"
-#     node = coords[int(name)]
-#     self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
-#     self.assertTrue(self.verifyNodeX(node, 1), name + " x is wrong")
-#   
-#   def testNodesToCheckForConflictingX(self):
-#     filePath = Utils.getAssetPath("test/setRelativeXToParent.json")
-#     nodeList = SaveManager.loadNodeDataListKeyConvertedToInt(filePath)
-#  
-#     reingold = ReingoldTilford()
-#       
-#     name = "3"
-#     node = nodeList[int(name)]
-#     self.assertTrue(reingold.checkForConflicts(node, nodeList, True), 
-#                     name + " is not being checked for conflict")
-#        
-#     name = "4"  # Main node
-#     node = nodeList[int(name)]
-#     self.assertTrue(reingold.checkForConflicts(node, nodeList, True), 
-#                     name + " is not being checked for conflict")
-#       
-#    
-#   def testContourList(self):
-#     filePath = Utils.getAssetPath("test/setRelativeXToParent.json")
-#     nodeList = SaveManager.loadNodeDataListKeyConvertedToInt(filePath)
-#        
-#     reingold = ReingoldTilford()
-#     coords, checkedForConflictIds = reingold.setInitialX(nodeList[1], nodeList)
-#       
-#     self.checkContourNode3(reingold, coords)
-#     self.checkContourNode4(reingold, coords)
-#       
-#   def checkContourNode3(self, reingold, coords):
-#     # Node 2 (Left node)
-#     # Right contour
-#     # Depth 2 == 1(Node 3)
-#     # Depth 3 == 2(Node 7)
-#      
-#     # Node 3 (Current Node)
-#     # Left contour
-#     # Depth 2 == 2(Node 4)
-#     # Depth 3 == 2(Node 8)
-#      
-#     name = "3"
-#     node = coords[int(name)]
-#        
-#     leftSibling = reingold.getLeftSibling(node, coords)
-#     rightContour = {}
-#     reingold.getRightContour(leftSibling, coords, 0, rightContour)
-#        
-#     depth = "2"
-#     self.assertTrue(rightContour.get(depth) == 1,  "node " + name + " " + 
-#                     str(depth) + " depth right contour is wrong ")
-#        
-#     depth = "3"
-#     self.assertTrue(rightContour[depth] == 2, "node " + name + " " + 
-#                     str(depth) + " depth right contour is wrong ")
-#        
-#     leftContour = {}
-#     reingold.getLeftContour(node, coords, 0, leftContour)
-#        
-#     depth = "2"
-#     expectedXPlusNodeSize = 2 # Because of this.x + nodeSize (1 + 1) 
-#     self.assertTrue(leftContour[depth] == expectedXPlusNodeSize, "node " + name + " " + 
-#                     str(depth) + " depth left contour is wrong ")
-#        
-#     depth = "3"
-#     expectedXPlusMod = 2 # Because of parent.x - this.x (2 - 0)
-#     self.assertTrue(leftContour[depth] == expectedXPlusMod, "node " + name + " " + 
-#                     str(depth) + " depth left contour is wrong ")
-#       
-#   def checkContourNode4(self, reingold, coords):
-#     # Node 3 (Left node)
-#     # Right contour
-#     # Depth 2 == 2(Node 3)
-#     # Depth 3 == 2(Node 8) (0 + Mod(2))
-#      
-#     # Node 4 (Current Node)
-#     # Left contour
-#     # Depth 2 == 3(Node 4)
-#     # Depth 3 == 2.5(Node 9)(0 + Mod(2.5))
-#      
-#     name = "4"
-#     node = coords[int(name)]
-#        
-#     leftSibling = reingold.getLeftSibling(node, coords)
-#     rightContour = {}
-#     reingold.getRightContour(leftSibling, coords, 0, rightContour)
-#        
-#     depth = "2"
-#     self.assertTrue(rightContour[depth] == 2,  "node " + name + " " + 
-#                     str(depth) + " depth right contour is wrong ")
-#        
-#     depth = "3"
-#     self.assertTrue(rightContour[depth] == 2, "node " + name + " " + 
-#                     str(depth) + " depth right contour is wrong ")
-#        
-#     leftContour = {}
-#     reingold.getLeftContour(node, coords, 0, leftContour)
-#        
-#     depth = "2"
-#     expectedXPlusNodeSize = 2 # Because of this.x + nodeSize (1 + 1) 
-#     self.assertTrue(leftContour[depth] == 3, "node " + name + " " + 
-#                     str(depth) + " depth left contour is wrong ")
-#        
-#     depth = "3"
-#     self.assertTrue(leftContour[depth] == 2.5, "node " + name + " " + 
-#                     str(depth) + " depth left contour is wrong ")
+  def testLocalXRelativeToParentNode(self):
+    filePath = Utils.getAssetPath("test/setRelativeXToParent.json")
+    nodeList = SaveManager.loadNodeDataListKeyConvertedToInt(filePath)
+     
+    reingold = ReingoldTilford()
+    coords, checkedForConflictIds = reingold.setInitialX(nodeList[1], nodeList)
+       
+    name = "1"  # Main node
+    node = coords[int(name)]
+    self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
+    self.assertTrue(self.verifyNodeX(node, 2), name + " x is wrong")
+       
+    # Main node children
+    name = "2"
+    node = coords[int(name)]
+    self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
+    self.assertTrue(self.verifyNodeX(node, 1), name + " x is wrong")
+       
+    name = "3"
+    node = coords[int(name)]
+    self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
+    self.assertTrue(self.verifyNodeX(node, 2), name + " x is wrong")
+    self.assertTrue(self.verifyMod(node, 2), name + " mod is wrong")
+       
+    name = "4"
+    node = coords[int(name)]
+    self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
+    self.assertTrue(self.verifyNodeX(node, 3), name + " x is wrong")
+    self.assertTrue(self.verifyMod(node, 2.5), name + " mod is wrong")
+    # Main node children
+       
+    # Node 2 children
+    name = "5"
+    node = coords[int(name)]
+    self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
+    self.assertTrue(self.verifyNodeX(node, 0), name + " x is wrong")
+       
+    name = "6"
+    node = coords[int(name)]
+    self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
+    self.assertTrue(self.verifyNodeX(node, 1), name + " x is wrong")
+       
+    name = "7"
+    node = coords[int(name)]
+    self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
+    self.assertTrue(self.verifyNodeX(node, 2), name + " x is wrong")
+    # Node 2 children
+       
+    # Node 3 child
+    name = "8"
+    node = coords[int(name)]
+    self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
+    self.assertTrue(self.verifyNodeX(node, 0), name + " x is wrong")
+       
+       
+    # Node 4 children
+    name = "9"
+    node = coords[int(name)]
+    self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
+    self.assertTrue(self.verifyNodeX(node, 0), name + " x is wrong")
+       
+    name = "10"
+    node = coords[int(name)]
+    self.assertTrue(self.verifyNodeName(node, name),  name + " name is wrong")
+    self.assertTrue(self.verifyNodeX(node, 1), name + " x is wrong")
+   
+  def testNodesToCheckForConflictingX(self):
+    filePath = Utils.getAssetPath("test/setRelativeXToParent.json")
+    nodeList = SaveManager.loadNodeDataListKeyConvertedToInt(filePath)
+  
+    reingold = ReingoldTilford()
+       
+    name = "3"
+    node = nodeList[int(name)]
+    self.assertTrue(reingold.checkForConflicts(node, nodeList, True), 
+                    name + " is not being checked for conflict")
+        
+    name = "4"  # Main node
+    node = nodeList[int(name)]
+    self.assertTrue(reingold.checkForConflicts(node, nodeList, True), 
+                    name + " is not being checked for conflict")
+       
+    
+  def testContourList(self):
+    filePath = Utils.getAssetPath("test/setRelativeXToParent.json")
+    nodeList = SaveManager.loadNodeDataListKeyConvertedToInt(filePath)
+        
+    reingold = ReingoldTilford()
+    coords, checkedForConflictIds = reingold.setInitialX(nodeList[1], nodeList)
+       
+    self.checkContourNode3(reingold, coords)
+    self.checkContourNode4(reingold, coords)
+       
+  def checkContourNode3(self, reingold, coords):
+    # Node 2 (Left node)
+    # Right contour
+    # Depth 2 == 1(Node 3)
+    # Depth 3 == 2(Node 7)
+      
+    # Node 3 (Current Node)
+    # Left contour
+    # Depth 2 == 2(Node 4)
+    # Depth 3 == 2(Node 8)
+      
+    name = "3"
+    node = coords[int(name)]
+        
+    leftSibling = reingold.getLeftSibling(node, coords)
+    rightContour = {}
+    reingold.getRightContour(leftSibling, coords, 0, rightContour)
+        
+    depth = 2
+    self.assertTrue(rightContour.get(depth) == 1,  "node " + name + " " + 
+                    str(depth) + " depth right contour is wrong ")
+        
+    depth = 3
+    self.assertTrue(rightContour[depth] == 2, "node " + name + " " + 
+                    str(depth) + " depth right contour is wrong ")
+        
+    leftContour = {}
+    reingold.getLeftContour(node, coords, 0, leftContour)
+        
+    depth = 2
+    expectedXPlusNodeSize = 2 # Because of this.x + nodeSize (1 + 1) 
+    self.assertTrue(leftContour[depth] == expectedXPlusNodeSize, "node " + name + " " + 
+                    str(depth) + " depth left contour is wrong ")
+        
+    depth = 3
+    expectedXPlusMod = 2 # Because of parent.x - this.x (2 - 0)
+    self.assertTrue(leftContour[depth] == expectedXPlusMod, "node " + name + " " + 
+                    str(depth) + " depth left contour is wrong ")
+       
+  def checkContourNode4(self, reingold, coords):
+    # Node 3 (Left node)
+    # Right contour
+    # Depth 2 == 2(Node 3)
+    # Depth 3 == 2(Node 8) (0 + Mod(2))
+      
+    # Node 4 (Current Node)
+    # Left contour
+    # Depth 2 == 3(Node 4)
+    # Depth 3 == 2.5(Node 9)(0 + Mod(2.5))
+      
+    name = "4"
+    node = coords[int(name)]
+        
+    leftSibling = reingold.getLeftSibling(node, coords)
+    rightContour = {}
+    reingold.getRightContour(leftSibling, coords, 0, rightContour)
+        
+    depth = 2
+    self.assertTrue(rightContour[depth] == 2,  "node " + name + " " + 
+                    str(depth) + " depth right contour is wrong ")
+        
+    depth = 3
+    self.assertTrue(rightContour[depth] == 2, "node " + name + " " + 
+                    str(depth) + " depth right contour is wrong ")
+        
+    leftContour = {}
+    reingold.getLeftContour(node, coords, 0, leftContour)
+        
+    depth = 2
+    expectedXPlusNodeSize = 2 # Because of this.x + nodeSize (1 + 1) 
+    self.assertTrue(leftContour[depth] == 3, "node " + name + " " + 
+                    str(depth) + " depth left contour is wrong ")
+        
+    depth = 3
+    self.assertTrue(leftContour[depth] == 2.5, "node " + name + " " + 
+                    str(depth) + " depth left contour is wrong ")
      
    
   def testFixConflictingX(self):
