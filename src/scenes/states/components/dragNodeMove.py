6cc71c21b5fccefbe1d2d9a0095c18c7311426c9
Nickan <colete_nico@yahoo.com>
2019-01-01 07:27:00 +0800

    Initial refactor of node drag

--- src/scenes/states/components/dragNodeMove.py
+++ src/scenes/states/components/dragNodeMove.py
@@ -4,26 +4,9 @@ from utils.utils import Utils
 
 class DragNodeMove():
 
-  def __init__(self, map, onRelease):
-    self.map = map
-    self.onRelease = onRelease
-    self.nodeDrawing = map.getSelectedNodeDrawing()
-
-    self.defaultPos = self.nodeDrawing.mainNode.getPos()
-    mPos = Utils.getMousePosition(map.showBase)
+  def __init__(self, drawingPos, mPos):
+    self.defaultPos = drawingPos
     self.posDiff = self.defaultPos - mPos
-
-    self.newParentVisualCue = NewParentVisualCue(map)
-    self.initListeners()
-
-  def initListeners(self):
-    showBase = self.map.showBase
-    showBase.taskMgr.add(self.mouseMove, "mouseMove")
-    showBase.accept("mouse1-up", self.mouse1Up)
-
-  def mouseMove(self, task):
-    self.setNodeDrawingPos()
-    return Task.cont
 
   def mouse1Up(self):
     self.map.showBase.ignoreAll()
@@ -34,14 +17,10 @@ class DragNodeMove():
     if nearestNodeDrawing is None:
       self.nodeDrawing.mainNode.setPos(self.defaultPos)
 
-  def setNodeDrawingPos(self):
-    pos = self.nodeDrawing.mainNode.getPos()
-
-    mPos = Utils.getMousePosition(self.map.showBase)
+  def setNodeDrawingPos(self, drawing, mPos):
     if mPos is not None:
       newNodePos = mPos + self.posDiff
-      self.nodeDrawing.mainNode.setPos(newNodePos)
-      self.newParentVisualCue.draw(self.map)
+      drawing.mainNode.setPos(newNodePos)
 
   def resetPosToDefault(self):
     self.nodeDrawing.mainNode.setPos(self.defaultPos)
--- src/scenes/states/dragNodeState.py
+++ src/scenes/states/dragNodeState.py
@@ -1,7 +1,10 @@
+
 from .state import State
+from direct.task.Task import Task
 from scenes.mapComponents.nodeDataFilter import NodeDataFilter
 from scenes.mapComponents.nodeData import NodeData
 from scenes.states.components.dragNodeMove import DragNodeMove
+from scenes.states.components.newParentVisualCue import NewParentVisualCue
 from utils.utils import Utils
 
 import copy
@@ -13,14 +16,40 @@ class DragNodeState(State):
     self.map = map
     
   def enter(self):
-    self.dragNodeMove = DragNodeMove(self.map, self.onRelease)
+    self.newParentVisualCue = NewParentVisualCue()
+
+    self.initDragNodeMove()
+    self.initControls()
+
+  def initDragNodeMove(self):
+    map = self.map
+    drawing = map.getSelectedNodeDrawing()
+    drawingPos = drawing.mainNode.getPos()
+    mPos = Utils.getMousePosition(map.showBase)
+    self.dragNodeMove = DragNodeMove(drawingPos, mPos)
+
+  def initControls(self):
+    showBase = self.map.showBase
+    showBase.taskMgr.add(self.mouseMove, "mouseMove")
+    showBase.accept("mouse1-up", self.mouse1Up)
+
+  def mouseMove(self, task):
+    map = self.map
+    drawing = map.getSelectedNodeDrawing()
+
+    mPos = Utils.getMousePosition(map.showBase)
+    self.dragNodeMove.setNodeDrawingPos(drawing, mPos)
+
+    nm = map.nodeManager
+    # self.newParentVisualCue.draw(mPos, nm.allDrawingData)
+    return Task.cont
+
+  def mouse1Up(self):
+    self.map.dragOnRelease(nearestDrawing, self)
 
   def exit(self):
-    print("exit DragNodeState")
-
-
-  def onRelease(self, nearestDrawing):
-    self.map.dragOnRelease(nearestDrawing, self)
+    self.map.showBase.ignoreAll()
+    self.map.showBase.taskMgr.remove("mouseMove")
 
 
 
